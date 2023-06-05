61d72fcc7ce11d80d86876c190e7d5af2cf327df
Tech-Scholar <geo.a.filippov@gmail.com>
2023-06-05 10:03:28 -0400

    Time till disappear implemented for events.

--- Events.py
+++ Events.py
@@ -9,13 +9,18 @@ class EventList:
     def add_event(self, event):
         self.eventList.append(event)
 
+    def remove_event(self, event):
+        self.eventList.remove(event)
+
     def clear_all(self):
         self.eventList.clear()
 
     def draw_events(self):
         for i in self.eventList:
             i.draw(self.screen)
-
+            i.age()
+            if i.time == 0:
+                self.remove_event(i)
 
 class Event:
     def __init__(self, version, time, text):
@@ -27,3 +32,6 @@ class Event:
         font = pygame.font.SysFont("Arial", 36)
         txtsurf = font.render(self.text, True, (255, 255, 255))
         screen.blit(txtsurf, (200 - txtsurf.get_width() // 2, 150 - txtsurf.get_height() // 2))
+
+    def age(self):
+        self.time -= 1
