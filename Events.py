672f252fad745b5cb2afb9af05081add93b3f579
Tech-Scholar <geo.a.filippov@gmail.com>
2023-06-06 10:59:08 -0400

    NPC can now has limited interactions. They can saw Hello.

--- Events.py
+++ Events.py
@@ -6,7 +6,8 @@ class EventList:
         self.screen = screen
         self.eventList = []
 
-    def add_event(self, event):
+    def add_event(self, event, owner):
+        event.owner = owner
         self.eventList.append(event)
 
     def remove_event(self, event):
@@ -14,24 +15,30 @@ class EventList:
 
     def clear_all(self):
         self.eventList.clear()
+
     #
     def draw_events(self):
         for i in self.eventList:
             i.draw(self.screen)
             i.age()
             if i.time == 0:
+                i.owner.hasBeenInteractedWith = False
                 self.remove_event(i)
 
+
 class Event:
-    def __init__(self, version, time, text):
+    def __init__(self, version, time, text, x, y):
         self.version = version
         self.time = time
         self.text = text
+        self.x = x
+        self.y = y
+        self.owner = None
 
     def draw(self, screen):
-        font = pygame.font.SysFont("Arial", 36)
+        font = pygame.font.SysFont("Arial", 24)
         txtsurf = font.render(self.text, True, (255, 255, 255))
-        screen.blit(txtsurf, (200 - txtsurf.get_width() // 2, 150 - txtsurf.get_height() // 2))
+        screen.blit(txtsurf, (self.x, self.y - 30))
 
     def age(self):
         self.time -= 1
--- ObstacleTypes/NPC.py
+++ ObstacleTypes/NPC.py
@@ -5,9 +5,13 @@ from Events import *
 
 class NPC(Obstacle):
     def __init__(self, x, y):
+        self.hasBeenInteractedWith = False
         super().__init__(x, y, 'NPC', pygame.image.load("Images/npc_front.png"), False)
 
     def interact(self, eventList):
-        new_event = Event('text', 10, 'Hello!')
-        eventList.add_event(new_event)
+        if not self.hasBeenInteractedWith:
+            self.hasBeenInteractedWith = True
+            new_event = Event('text', 10, 'Hello!', self.x, self.y)
+            eventList.add_event(new_event, self)
+
 
